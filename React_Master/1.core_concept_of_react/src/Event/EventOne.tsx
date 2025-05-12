573807c7d1f16b4e8d7e6b4d950238e69d7cae2f
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-05-12 11:14:12 +0545

    event

---
+++ React_Master/1.core_concept_of_react/src/Event/EventOne.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EventOne = () => {
+  return (
+    <div>
+      <h1>This is all about the Event...</h1>
+      <button onClick={() => alert("hello you...")}>Click Me</button>
+    </div>
+  );
+};
+
+export default EventOne;

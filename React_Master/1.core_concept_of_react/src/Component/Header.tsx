632bfc8b5c881498cc8a857a9fd08e2d2a0e79d6
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-05-12 11:11:55 +0545

    component

---
+++ React_Master/1.core_concept_of_react/src/Component/Header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Project</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ React_Master/1.core_concept_of_react/src/Component/Home.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is Home Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+        exercitationem quis vel nam ut doloribus dolorum dolorem. Eaque
+        voluptatum vero, possimus odio recusandae maxime atque optio, quod nemo
+        voluptatibus tempore quasi repudiandae sed incidunt nobis dolorem
+        exercitationem, deleniti excepturi adipisci ab maiores neque.
+        Voluptatibus amet dolores repellat sapiente nulla quibusdam iure impedit
+        hic sit, quasi provident velit est voluptate ex, nostrum laborum
+        accusamus laboriosam doloribus cupiditate quam possimus blanditiis
+        labore. Nihil dignissimos voluptatem tempora eveniet! Molestiae
+        aspernatur placeat doloribus ut, voluptate, sapiente eos illum pariatur
+        excepturi labore illo modi provident voluptatum eligendi impedit debitis
+        assumenda? Laudantium eos quo obcaecati atque?
+      </p>
+    </div>
+  );
+};
+
+export default Home;

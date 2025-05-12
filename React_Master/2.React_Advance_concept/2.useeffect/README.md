98c04fa07a4a6a989dee2088e60679a9ce7a8f8b
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-05-12 19:18:23 +0545

    useeffect

---
+++ React_Master/2.React_Advance_concept/2.useeffect/README.md
@@ -0,0 +1,3 @@
+# useEffect Hook
+
+- useEffect Hook is used to fetch the api as well as side effects of component.
---
+++ React_Master/2.React_Advance_concept/2.useeffect/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ExampleOne from "./Component/ExampleOne";
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is all about the data fetch method.</h1>
+      <ExampleOne />
+    </div>
+  );
+};
+
+export default App;

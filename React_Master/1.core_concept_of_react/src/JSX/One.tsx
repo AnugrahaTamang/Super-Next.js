b889eba31d57d2d018b24dd464ddd65b36778621
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-05-12 11:12:52 +0545

    jsx

---
+++ React_Master/1.core_concept_of_react/src/JSX/One.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const JSX = () => {
+  const name: string = "Anugraha Tamang";
+  const age: number = 22;
+  const location: string = "Nepal";
+  return (
+    <div>
+      <h1>
+        My name is {name}. I am {age} years old. I am {location}
+      </h1>
+    </div>
+  );
+};
+
+export default JSX;

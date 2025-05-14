af8041be7cb5d97e0f90ea6c66238cb4b3ccd6c2
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-05-14 20:52:01 +0545

    src

---
+++ React_Master/2.React_Advance_concept/5.useReducer/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Counter from "./Component/Counter";
+import Form from "./Component/Form";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Simple Couter App:</h1>
+      <Counter />
+      <Form />
+    </div>
+  );
+};
+
+export default App;

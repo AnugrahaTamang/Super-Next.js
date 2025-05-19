3a21b4095f053e7e75ca59eb0e721db0e2d6c1c4
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-05-19 21:14:07 +0545

    intercepting

--- 2.Advance Routing/intercepting/README.md
+++ 2.Advance Routing/intercepting/README.md
@@ -1,1 +1,5 @@
 # Intercepting route
+
+- isplay a modal or overlay instead of navigating to a new page.
+
+Override default routing behavior without losing the original context (like keeping the background content visible).
--- 2.Advance Routing/intercepting/app/ecommerce/page.tsx
+++ 2.Advance Routing/intercepting/app/ecommerce/page.tsx
@@ -1,6 +1,7 @@
+import Link from "next/link";
 import React from "react";
 
-const Home = () => {
+const Ecommerce = () => {
   const products = [
     {
       id: 1,
@@ -24,7 +25,26 @@ const Home = () => {
         "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
   ];
-  return <div>Home</div>;
+  return (
+    <div>
+      <section className="m-[4rem]">
+        <div>
+          <h1 className="text-3xl mb-3">Ecommerce</h1>
+        </div>
+        <section className="flex gap-3">
+          {products.map((product) => (
+            <div className="w-[200px] border" key={product.id}>
+              <Link href={`/ecommerce/${product.id}`}>
+                <img src={product.image} alt={product.name} />
+                <h2>{product.name}</h2>
+                <h2>{product.price}</h2>
+              </Link>
+            </div>
+          ))}
+        </section>
+      </section>
+    </div>
+  );
 };
 
-export default Home;
+export default Ecommerce;
